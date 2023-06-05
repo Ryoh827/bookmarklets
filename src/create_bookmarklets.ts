a9461a9d862a00d9469b4f09efab024b13c0c729
Ryoh827 <rn.gjt25@gmail.com>
2023-06-05 22:57:00 +0900

    feat: tidy up files

---
+++ src/create_bookmarklets.ts
@@ -0,0 +1,5 @@
+/**
+ * src上のpath一覧を取得する
+ * @return {string[]} path一覧 (ex. ['src/index.ts', 'src/xxx.ts'])
+ */
+const getSrcPaths = (): string[] => {};
--- src/sample.ts
+++
@@ -1,3 +0,0 @@
-(async function sample() {
-  alert('sample');
-})();
